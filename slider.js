4dafcbfb9c85693ca9eb45d4d9a5346d77f17d8d
unknown <is686863@iteso.mx>
2016-03-22 09:50:33 -0600

    Proyecto de Slider

---
+++ slider.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+
+// Place the JQuery Javascript here //
+
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() { 
+  $('#slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+},  3000);
+
+$("#slideshow2 > div:gt(0)").hide();
+
+setInterval(function() { 
+  $('#slideshow2 > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow2');
+},  3000);
+
+
+
+});
---
+++ slider2.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+// Place the JQuery Javascript here //
+
+$("#slideshow2 > div:gt(0)").hide();
+
+setInterval(function() { 
+  $('#slideshow2 > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow2');
+},  3000);
+
+
+});
